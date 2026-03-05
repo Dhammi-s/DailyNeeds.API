b8aa233e7e52c03a8354565d0da75e31a7da5697
jasmeet singh <jasmeet.singh@caresmartz360.com>
2026-03-05 18:43:24 +0530

    add

--- ServicePro.Core/Interfaces/IEmployeeService.cs
+++ ServicePro.Core/Interfaces/IEmployeeService.cs
@@ -10,7 +10,7 @@ namespace ServicePro.Core.Interfaces
 {
     public interface IEmployeeService
     {
-
+        //testcomments
         Task<List<EmployeeResponseDto>> GetAllEmployeesAsync();
 
     }
