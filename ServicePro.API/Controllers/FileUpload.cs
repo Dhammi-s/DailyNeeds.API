c773097c790bcc02cefdc2b4fb36cce4f5820b83
jasmeet singh <jasmeet.singh@caresmartz360.com>
2026-04-20 17:40:14 +0530

    ADD

---
+++ ServicePro.API/Controllers/FileUpload.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicePro.Core.DTOs.Inbound;
+using ServicePro.Core.Interfaces;
+
+namespace ServicePro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUpload : ControllerBase
+    {
+        public readonly IFileUpload _FileUpload;
+        public FileUpload(IFileUpload FileUpload)
+        {
+            _FileUpload = FileUpload;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile([FromBody] FileUploadRequestDTO file)
+        {
+            if (file == null )
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            var result = await _FileUpload.UploadFileAsync(file);
+            if (!result)
+            {
+                return StatusCode(500, "Error uploading file.");
+            }
+
+            return Ok("File uploaded successfully.");
+        }
+    }
+}
---
+++ ServicePro.Core/Interfaces/IFileUpload.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using ServicePro.Core.DTOs.Inbound;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicePro.Core.Interfaces
+{
+    public interface IFileUpload
+    {
+        Task<bool> UploadFileAsync(FileUploadRequestDTO file);
+    }
+}
